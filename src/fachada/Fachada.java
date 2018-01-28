699571cacd71c6be715c8e8d53943fcfca3059e9
Junior Paiva <juniorpaiva95@gmail.com>
2018-01-28 01:08:38 -0200

    Estrutura do Projeto
    Estrutura do Projeto

--- src/fachada/Fachada.java
+++ src/fachada/Fachada.java
@@ -13,21 +13,7 @@ import daojpa.DAO;
 import daojpa.*;
 import modelo.*;
 
-/**********************************
- * IFPB - Curso Superior de Tec. em Sist. para Internet
- * Persistencia de Objetos
- * Prof. Fausto Ayres
- *
- */
-
 public class Fachada {
-	
-	private static DAOFuncionario daofuncionario = new DAOFuncionario();
-	private static DAOCargo daocargo = new DAOCargo();
-	private static DAOFilme daofilme = new DAOFilme();
-	private static DAOSala daosala = new DAOSala();
-	private static DAOSessao daosessao = new DAOSessao();
-	private static DAOHorario daohorario = new DAOHorario();
 	
 	public static void inicializar(){
 		DAO.abrir();
